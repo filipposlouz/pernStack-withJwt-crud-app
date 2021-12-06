487510c4a9e592ca034d48d13cbd2a54917e4d3d
filipposlouz <63556263+filipposlouz@users.noreply.github.com>
2021-12-07 01:10:22 +0200

    Delete employee.sql

--- employee.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE employees;
-
-CREATE TABLE employee (
-	id SERIAL NOT NULL PRIMARY KEY,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	afm VARCHAR(9) NOT NULL,
-	date_of_birth DATE NOT NULL
-);
