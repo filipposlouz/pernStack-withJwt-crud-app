ab3319a3f66e76ddd5e8bfc1ed792f0a2c52d9c9
filipposlouz <63556263+filipposlouz@users.noreply.github.com>
2021-12-09 02:06:16 +0200

    Add files via upload

--- client/src/components/inputEmployee.js
+++ client/src/components/inputEmployee.js
@@ -1,8 +1,6 @@
 import React, { Fragment, useState } from "react";
 import Modal from "./modal";
 
-// FIX EMPTY VALUES CASE
-
 const InputEmployee = () => {
   const [employee, setEmployee] = useState({
     first_name: "",
@@ -68,87 +66,6 @@ const InputEmployee = () => {
       {showModal.isModalOpen && (
         <Modal closeModal={closeModal} modalContent={showModal.modalContent} />
       )}
-      {/* <form
-        style={{ display: "flex", flexDirection: "row", marginTop: "5rem" }}
-      >
-        <div className="form-control">
-          <label htmlFor="first_name">
-            <h4>
-              First Name :
-              <input
-                type="text"
-                className="form-control"
-                id="first_name"
-                name="first_name"
-                value={employee.first_name}
-                onChange={handleChange}
-              />
-            </h4>
-          </label>
-        </div>
-        <div className="form-control">
-          <label htmlFor="last_name">
-            <h4>
-              Last Name :
-              <input
-                type="text"
-                className="form-control"
-                id="last_name"
-                name="last_name"
-                value={employee.last_name}
-                onChange={handleChange}
-              />
-            </h4>
-          </label>
-        </div>
-        <div className="form-control">
-          <label htmlFor="afm">
-            <h4>
-              AFM :
-              <input
-                type="text"
-                className="form-control"
-                id="afm"
-                name="afm"
-                value={employee.afm}
-                onChange={handleChange}
-              />
-            </h4>
-          </label>
-        </div>
-        <div className="form-control">
-          <label htmlFor="date_of_birth">
-            <h4>
-              Date Of Birth :
-              <input
-                type="text"
-                className="form-control"
-                id="date_of_birth"
-                name="date_of_birth"
-                value={employee.date_of_birth}
-                onChange={handleChange}
-              />
-            </h4>
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="btn btn-success"
-          onClick={handleSubmit}
-        >
-          Add
-        </button>
-      </form>
-      {employees.map((person, index) => {
-        const { first_name, last_name, afm, date_of_birth } = person;
-        return (
-          <li key={index} className="mt-5">
-            <h4>
-              {first_name} {last_name} {afm} {date_of_birth}
-            </h4>
-          </li>
-        );
-      })} */}
       <table className="table" style={{ marginTop: "5rem" }}>
         <thead>
           <tr>
